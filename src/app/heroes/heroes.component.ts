69895bfce602e4fb6bb37611887fdbcd1e851068
Julia Medvedeva <medvedevajulev@gmail.com>
2023-06-08 13:24:02 +0300

    wip: создала компонент с деталями для героя

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,13 +8,9 @@ import { HEROES } from 'src/mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
-
   selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
